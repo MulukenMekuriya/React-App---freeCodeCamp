89a61e26a6513207f82b5895005cf00c0b3e2cb7
Muluken <mulerthegrete@gmail.com>
2021-11-15 09:04:03 -0500

    working with props and props.children

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// CSS
 import './index.css'
 
+const firstBook = {
+  title: 'Will',
+  author: 'Will Smith',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91PpDVWjhLL._AC_UL200_SR200,200_.jpg',
+}
+
+const secondBook = {
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+}
 const BookList = () => {
   // return React.createElement('h1', {}, 'hellow world')
   return (
     <section className='bookList'>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+          soluta reiciendis aliquid atque error? Id sunt consequuntur molestias
+          exercitationem placeat consectetur animi autem sint tenetur cum ut
+          quo, facilis recusandae?
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book />
       <Book />
       <Book />
       <Book />
@@ -22,31 +52,16 @@ const BookList = () => {
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
     </article>
   )
-}
-
-const Image = () => {
-  return (
-    <img
-      src='https://images-na.ssl-images-amazon.com/images/I/91PpDVWjhLL._AC_UL200_SR200,200_.jpg'
-      alt=''
-    />
-  )
-}
-
-const Title = () => {
-  return <h1>Will</h1>
-}
-
-const Author = () => {
-  return <h1>Will Smith</h1>
 }
 
 ReactDOM.render(<BookList />, document.getElementById('root'))
